89a71b9fecc0610ff6030b61918eac2d0ecc9230
Roy Feng <roy.feng@FVFYLWN1J1WK.local>
2021-11-25 10:53:01 -0500

    multiple lines on one graph

--- FinalGraph_Everything.py
+++ FinalGraph_Everything.py
@@ -9,8 +9,6 @@ import statistics as stat
 dataPercents = loadList("FinalPercents.csv")
 dataHeightsWeights = loadList("FinalHeightsWeights.csv")
 
-
-
 dict = {}
 
 col = {
@@ -18,7 +16,6 @@ col = {
     "FG": [12, 'masterListFG'],
     "3PT": [14, 'masterList3PT']
 }
-
 
 def getMasterList(index, L):
     global col 
@@ -49,7 +46,6 @@ def getMasterList(index, L):
 
     graphList = []
 
-
     for key,value in dictL.items():
         sum = 0
         average = 0
@@ -63,18 +59,47 @@ def getMasterList(index, L):
 
     return graphList
 
-
-
-
 masterListFT = getMasterList(col['FT'][0], [])
 masterListFG = getMasterList(col['FG'][0], [])
 masterList3PT = getMasterList(col['3PT'][0], [])
-print('this is ft')
-for i in masterListFT: 
-    print(i)
-print('this is fg')
-for i in masterListFG: 
-    print(i)
-print('this is 3pt')
-for i in masterList3PT: 
-    print(i)
+
+masterListFT.sort()
+masterListFG.sort()
+masterList3PT.sort()
+
+#graphing: 
+#----------------------------------------------------------------------------------------------------------------
+
+dfFT = pd.DataFrame(masterListFT, columns= ["Height", "FT"])
+dfFG = pd.DataFrame(masterListFG, columns= ["Height", "FG"])
+df3PT = pd.DataFrame(masterList3PT, columns= ["Height", "THREE"])
+
+m, b = np.polyfit(dfFT.Height, dfFT.FT, 1)
+fitEquationFT = m*dfFT.Height + b
+
+m, b = np.polyfit(dfFG.Height, dfFG.FG, 1)
+fitEquationFG = m*dfFG.Height + b
+
+m, b = np.polyfit(df3PT.Height, df3PT.THREE, 1)
+fitEquation3PT = m*df3PT.Height + b
+
+plt.scatter(dfFT.Height, dfFT.FT)
+plt.plot(dfFT.Height, dfFT.FT, "b")
+plt.plot(dfFT.Height, fitEquationFT, "--",color = "red")
+
+plt.scatter(dfFG.Height, dfFG.FG)
+plt.plot(dfFG.Height, dfFG.FG, "y")
+plt.plot(dfFG.Height, fitEquationFG, "--",color = "red")
+
+plt.scatter(df3PT.Height, df3PT.THREE)
+plt.plot(df3PT.Height, df3PT.THREE, "g")
+plt.plot(df3PT.Height, fitEquation3PT, "--",color = "red")
+
+plt.xticks(np.arange(69, 87, 1.0))
+plt.yticks(np.arange(0, 105, 5))
+
+plt.xlabel('Height (inches)', fontweight='bold')
+plt.ylabel('Percentage (%)', fontweight='bold')
+plt.title("Height VS Shooting Percentage %", fontweight="bold", fontsize=20)
+plt.show()
+
